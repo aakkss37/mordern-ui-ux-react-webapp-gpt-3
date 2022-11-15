60718dcbad70b2e3eccc165f4c27d45adede6405
amar kumar <amarkumar.sharma.124@gmail.com>
2022-11-15 21:31:25 +0530

    Navigation-bar created for all devices

---
+++ src/components/navbar/NavbarMenu.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const NavbarMenu = () => {
+  return (
+    <>
+        <p><a href='#home'>Home</a></p>
+        <p><a href='#wgpt3'>What is GPT</a></p>
+        <p><a href='#possibility'>Open AI</a></p>
+        <p><a href='#features'>Case study</a></p>
+        <p><a href='#blog'>Library</a></p>
+    </>
+  );
+};
+export default NavbarMenu;
