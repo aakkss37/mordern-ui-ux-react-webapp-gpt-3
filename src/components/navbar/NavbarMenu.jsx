ad82a3ac1fd4946ae0435184c6ed303ae6252bbe
amar kumar <amarkumar.sharma.124@gmail.com>
2022-11-16 13:40:58 +0530

    Footer section created

--- src/components/navbar/NavbarMenu.jsx
+++ src/components/navbar/NavbarMenu.jsx
@@ -6,7 +6,7 @@ export const NavbarMenu = () => {
         <p><a href='#home'>Home</a></p>
         <p><a href='#wgpt3'>What is GPT</a></p>
         <p><a href='#possibility'>Open AI</a></p>
-        <p><a href='#features'>Case study</a></p>
+        <p><a href='#future'>Case study</a></p>
         <p><a href='#blog'>Library</a></p>
     </>
   );
